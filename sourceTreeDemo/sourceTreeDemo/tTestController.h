abdf38e3f2bdde65d84810483308b994b3777cef
Haoran Liu <18318727569@163.com>
2018-05-16 15:15:54 +0800

    提交代码

--- sourceTreeDemo/sourceTreeDemo/tTestController.h
+++ sourceTreeDemo/sourceTreeDemo/tTestController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  tTestController.h
 //  sourceTreeDemo
 //
 //  Created by Haoran Liu on 2018/5/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface tTestController : UIViewController
 
 @end
-
---
+++ sourceTreeDemo/sourceTreeDemo/tTestController.m
@@ -0,0 +1,38 @@
+//
+//  tTestController.m
+//  sourceTreeDemo
+//
+//  Created by Haoran Liu on 2018/5/16.
+//  Copyright © 2018年 DaShenLin. All rights reserved.
+//
+
+#import "tTestController.h"
+
+@interface tTestController ()
+
+@end
+
+@implementation tTestController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"dev_1 创建分支准备");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
